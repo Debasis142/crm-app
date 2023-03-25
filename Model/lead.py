debc665cc1a3ecc9982ae6b55bf49df27b2262b4
Debasis Behera <debasis157.nalada@gmail.com>
2023-03-25 13:24:53 +0000

    Lead feature initialize *Create lead functionality done and tested.

--- Model/lead.py
+++ Model/lead.py
@@ -1,1 +1,18 @@
-lead.py
+# Lead model implimetaion
+
+# Define the lead data that you want to create
+lead_data = {
+    "LastName": "Doe",
+    "FirstName": "John",
+    "Company": "Acme Inc.",
+    "Email": "jdoe@acme.com"
+}
+
+# Send a POST request to create the lead
+response = requests.post(url, headers=headers, data=json.dumps(lead_data))
+
+# Check if the request was successful
+if response.status_code == 201:
+    print("Lead created successfully")
+else:
+    print("Failed to create lead. Status code:", response.status_code)
