b0a426ce8c4befd4c1e1645ec6e5331e69910156
Debasis Behera <debasis157.nalada@gmail.com>
2023-03-25 13:49:41 +0000

    Lead feature in progress *update lead andget lead done

--- Controller/load.py
+++ Controller/load.py
@@ -15,3 +15,22 @@ if response.status_code == 201:
     print("Lead created successfully")
 else:
     print("Failed to create lead. Status code:", response.status_code)
+
+
+#Lead update controller implimention
+# Define the lead data that you want to create
+lead_data = {
+    "LastName": "Doe",
+    "FirstName": "John",
+    "Company": "Acme Inc.",
+    "Email": "jdoe@acme.com"
+}
+
+# Send a POST request to create the lead
+response = requests.post(url, headers=headers, data=json.dumps(lead_data))
+
+# Check if the request was successful
+if response.status_code == 201:
+    print("Lead created successfully")
+else:
+    print("Failed to create lead. Status code:", response.status_code)
